b7ced2b49126f5826c6470f6e9bd953a6d4fadd2
randyputra <randyputra451@gmail.com>
2022-08-06 11:14:00 +0700

    change sensor variabel

--- model.py
+++ model.py
@@ -1,24 +1,38 @@
 import uuid
 from dataclasses import field
 from datetime import datetime
-
 from marshmallow_dataclass import dataclass as mm_dataclass
-from typing import Optional, List
+from typing import Optional
 from dataclasses_json import dataclass_json, Undefined
-from marshmallow import validate
-
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @mm_dataclass(frozen=True)
-class SensorModel:
-    status: bool
-    temperature: Optional[int]
-    latitude: Optional[float]
-    longitude: Optional[float]
-    movement: Optional[bool]
 
-    id: str = field(default_factory=lambda: str(uuid.uuid4())[:8])
-    created_at: datetime = field(metadata={
+### Format Data JSON
+# {
+#     "name": "Randy",
+#     "umur": 20
+# }
+###
+
+class SensorModel:
+    #Variabel ajib dikirim ke server
+    status: bool #true or false
+    speed: float #0.001
+    latitude: float
+    longitude: float
+
+    #Variabel opsional boleh dikirim boleh ngga
+    movement: Optional[bool] 
+    
+    #Autogenerate by Database
+    created_at: datetime = field(metadata={ #add timestamp
         'dataclasses_json': {
             'encoder': lambda x: datetime.timestamp(x),
         }
     }, default_factory=datetime.utcnow)
+
+    # id: str = field(default_factory=lambda: str(uuid.uuid4())[:8])
+
+class UserModel:
+    name: str
+    age: int
